6ece5343a1615752c600944596e2ea37d5276705
shaka2-dev <atama.41414444@gmail.com>
2026-06-01 16:45:25 +0900

    Shaka2 PWA files

---
+++ sw.js
@@ -0,0 +1,54 @@
+const CACHE_NAME = 'shaka2-pwa-v3';
+const ASSETS_TO_CACHE = [
+  './',
+  './shaka2-18_PWA.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  console.log('Service Worker installed');
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  console.log('Service Worker activated');
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request).then(response => {
+        if (response.ok) {
+          const responseClone = response.clone();
+          caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request, responseClone);
+          });
+        }
+        return response;
+      });
+    }).catch(() => {
+      return caches.match('./shaka2-18_PWA.html');
+    })
+  );
+});
